9da2b9c8fee87defaad2ef05a7cc3bcd4aade20e
Joel Pelletier <joel.pelletier@jpelletier.com>
2023-04-30 20:10:52 -0400

    Missions123
    added basic structure for 3 missions to Pizza Place, Construction Site, and Parking Garage

--- Assets/CharacterController3D.cs
+++ Assets/CharacterController3D.cs
@@ -29,10 +29,19 @@ public class CharacterController3D : MonoBehaviour
     float drag;
     float reset;
 
+    //upturn fallen over bird
+    //[SerializeField] private float angleThreshold = 45f; // The angle threshold to consider the object on its side
+    [SerializeField] private float rotationSpeed = 1f; // The speed at which the object rotates to the upright position
+
     public GameObject mail;
-    public GameObject missiontarget;
+    public GameObject boweryTarget;
+    public GameObject missiontarget1;
+    public GameObject missiontarget2;
+    public GameObject missiontarget3;
     public GameObject bowerycanvas;
-    public GameObject missioncanvas;
+    public GameObject missioncanvas1;
+    public GameObject missioncanvas2;
+    public GameObject missioncanvas3;
     public GameObject deathcanvas;
 
     //Sound References
@@ -55,8 +64,12 @@ public class CharacterController3D : MonoBehaviour
     public float pitchDownForce = -1f;
     public float maxForceMagnitude = 10f;
 
+    //Mission progress
+    int missionProgress;
+
     void Start()
     {
+        missionProgress = 1;
         m_Rigidbody = GetComponent<Rigidbody>();
 
         newPosition = transform.position;
@@ -81,7 +94,7 @@ public class CharacterController3D : MonoBehaviour
             tilt = ctx.ReadValue<Vector2>();
 
         };
-        controls.gameplay.move.canceled += ctx => slidemove = Vector2.zero;
+        //controls.gameplay.move.canceled += ctx => slidemove = Vector2.zero;
         controls.gameplay.move.performed += ctx =>
         {
             slidemove = ctx.ReadValue<Vector2>();
@@ -101,11 +114,25 @@ public class CharacterController3D : MonoBehaviour
             {
                 bowerycanvas.SetActive(false);
                 acceptmissionsound.Play();
+                UnfreezePosition();
             }
-            if (missioncanvas.activeInHierarchy)
+            if (missioncanvas1.activeInHierarchy)
             {
-                missioncanvas.SetActive(false);
+                missioncanvas1.SetActive(false);
                 completemissionsound.Play();
+                UnfreezePosition();
+            }
+            if (missioncanvas2.activeInHierarchy)
+            {
+                missioncanvas2.SetActive(false);
+                completemissionsound.Play();
+                UnfreezePosition();
+            }
+            if (missioncanvas3.activeInHierarchy)
+            {
+                missioncanvas3.SetActive(false);
+                completemissionsound.Play();
+                UnfreezePosition();
             }
             flapsound.Play();
             isGrounded = false;
@@ -165,7 +192,25 @@ public class CharacterController3D : MonoBehaviour
         flaplock = true;
         flaplocktimeout = 0.3f;
     }
- 
+
+    public bool IsSideways()
+    {
+        // Get the rotation angles of the target GameObject
+        Vector3 rotationAngles = gameObject.transform.eulerAngles;
+        //Debug.Log($"Checking X:{rotationAngles.x} Z:{rotationAngles.z}");
+        // Check if the GameObject has been rotated 90 degrees or more around 2 axis
+        if ((Mathf.Abs(rotationAngles.x) >= 88 && Mathf.Abs(rotationAngles.x) <= 272) || (Mathf.Abs(rotationAngles.z) >= 88 && Mathf.Abs(rotationAngles.z) <= 272))
+        {
+            //Debug.Log($"The GameObject is rotated 88 degrees or more. X:{rotationAngles.x} Z:{rotationAngles.z}");
+            return true;
+        }
+        else
+        {
+            //Debug.Log("The GameObject is rotated less than 88 degrees.");
+            return false;
+        }
+    }
+
     void FixedUpdate()
     {
         // Angle of Attack based movement
@@ -180,7 +225,7 @@ public class CharacterController3D : MonoBehaviour
         {
             forceMagnitude = Mathf.Clamp(angle * pitchDownForce, -maxForceMagnitude, 0f);
         }
-        m_Rigidbody.AddForce(forceDirection * forceMagnitude * 0.00001f);
+        m_Rigidbody.AddForce(forceDirection * forceMagnitude * 0.00002f);
 
         if (flaplocktimeout <= 0)
         {
@@ -192,21 +237,8 @@ public class CharacterController3D : MonoBehaviour
         m_Rigidbody.AddTorque(transform.right * tilt.y * 0.00002f * m_Thrust); //pitch
         if (!flaplock)
         { 
-            //Apply input values as forces against each thruster group
-            //m_Rigidbody.AddForce(transform.forward * slidemove.y * 0.05f * m_Thrust, ForceMode.Impulse); //Slide vertical
-            m_Rigidbody.AddForce(transform.right * slidemove.x * 0.05f * m_Thrust, ForceMode.Impulse); //slide horizontal
+            m_Rigidbody.AddForce(transform.right * slidemove.x * 0.01f * m_Thrust, ForceMode.Impulse); //slide horizontal
             m_Rigidbody.AddForce(transform.up * lift * 0.01f * m_Thrust, ForceMode.Impulse); //Add up lift
-            //m_Rigidbody.AddTorque(transform.right * pitchyaw.y * 0.1f * m_Thrust); //pitch
-        
-
-            //m_Rigidbody.AddForce(transform.forward * forward * 1f * m_Thrust); //forward
-            //m_Rigidbody.AddForce(transform.forward * backwards * -1f * m_Thrust); //backwards
-            //m_Rigidbody.AddTorque(transform.forward * rollright * -0.05f * m_Thrust); //rollright
-            //m_Rigidbody.AddTorque(transform.forward * rollleft * 0.05f * m_Thrust); //rollleft
-            //m_Rigidbody.drag = drag; //brake
-            //m_Rigidbody.angularDrag = drag; //brake
-            //if some sort of state is true then call SetFlaplock()
-            
         }
         else { flaplocktimeout = flaplocktimeout - 0.1f; }
  
@@ -226,6 +258,17 @@ public class CharacterController3D : MonoBehaviour
         m_Rigidbody.velocity = velocity.normalized * speed;
 
         //isGrounded = Physics.Raycast(transform.position, Vector3.down, groundDistance);
+        if (isGrounded && IsSideways())
+        {
+            SmoothUpturn();
+            if (speed <= 0.03f)
+            {
+                Debug.Log($"Sideways Stuck!!!, speed is {speed}, limit is 0.03");
+                RaiseObjectBy(1.0f);
+                transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);                
+                isGrounded = false;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -246,16 +289,117 @@ public class CharacterController3D : MonoBehaviour
         if (collision.gameObject.name == "TheBowery")
         {
             mail.SetActive(true);
-            missiontarget.SetActive(true);
+            if (missionProgress == 1)
+            {
+                missiontarget1.SetActive(true);
+            }
+            else if (missionProgress == 2) 
+            {
+                missiontarget2.SetActive(true);
+            }
+            else if (missionProgress == 3)
+            {
+                missiontarget3.SetActive(true);
+            }
             bowerycanvas.SetActive(true);
+            FreezePosition();
+            boweryTarget.SetActive(false);
+            missionProgress++;
         }
-        if (collision.gameObject.name == "MissionTarget")
+        if (collision.gameObject.name == "MissionTarget1")
         {
-            missiontarget.SetActive(false);
-            missioncanvas.SetActive(true);
+            missiontarget1.SetActive(false);
+            missioncanvas1.SetActive(true);
             mail.SetActive(false);
+            FreezePosition();
+            boweryTarget.SetActive(true);
+        }
+        if (collision.gameObject.name == "MissionTarget2")
+        {
+            missiontarget2.SetActive(false);
+            missioncanvas2.SetActive(true);
+            mail.SetActive(false);
+            FreezePosition();
+            boweryTarget.SetActive(true);
+        }
+        if (collision.gameObject.name == "MissionTarget3")
+        {
+            missiontarget3.SetActive(false);
+            missioncanvas3.SetActive(true);
+            mail.SetActive(false);
+            FreezePosition();
+            boweryTarget.SetActive(true);
         }
     }
-    
+    void SmoothUpturn()
+    {
+        // Calculate the target rotation angles for the upright position
+        Vector3 currentEulerAngles = transform.eulerAngles;
+        float xRotation = (currentEulerAngles.x > 180) ? currentEulerAngles.x - 360 : currentEulerAngles.x;
+        float zRotation = (currentEulerAngles.z > 180) ? currentEulerAngles.z - 360 : currentEulerAngles.z;
+
+        // Determine if x or z rotation is larger in magnitude
+        bool useXRotation = Mathf.Abs(xRotation) > Mathf.Abs(zRotation);
+
+        // Set the rotation to the closest multiple of 90 degrees
+        float targetXRotation = useXRotation ? Mathf.Round(xRotation / 90) * 90 : 0;
+        float targetZRotation = useXRotation ? 0 : Mathf.Round(zRotation / 90) * 90;
+
+        // Calculate the target rotation as a Quaternion
+        Quaternion targetRotation = Quaternion.Euler(targetXRotation, currentEulerAngles.y, targetZRotation);
+
+        // Smoothly rotate the object to the upright position using Slerp
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    bool IsUpsideDownStationary()
+    {
+        // Get the current rotation angles of the object
+        Vector3 rotationAngles = transform.eulerAngles;
+
+        // Normalize the angles to the range of [0, 360)
+        rotationAngles.y = rotationAngles.y % 360;
+
+        // Check if the object is upside down based on the angle threshold
+        bool isUpsideDown = Mathf.Abs(rotationAngles.y) > 180 - 180 && Mathf.Abs(rotationAngles.y) < 180 + 180;
+
+        // Check if the object's velocity is below the stationary threshold
+        bool isStationary = m_Rigidbody.velocity.magnitude < 0.1f;
+
+        // Return true if the object is upside down and stationary
+        return isUpsideDown && isStationary;
+    }
+
+    public void RaiseObjectBy(float raiseAmount)
+    {
+        // Get the current position of the object
+        Vector3 currentPosition = transform.position;
+
+        // Raise the object by the specified raiseAmount on the y-axis
+        currentPosition.y += raiseAmount;
+
+        // Apply the updated position to the object
+        transform.position = currentPosition;
+    }
+
+    public void FreezePosition()
+    {
+        // Store the current velocity before freezing
+        //storedVelocity = rb.velocity;
+
+        // Freeze the position of the Rigidbody along the x, y, and z axes
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    public void UnfreezePosition()
+    {
+        // Unfreeze the position of the Rigidbody along the x, y, and z axes
+        m_Rigidbody.constraints = RigidbodyConstraints.None;
+
+        // Reapply the stored velocity to the Rigidbody
+        //rb.velocity = storedVelocity;
+    }
+
 
 }
